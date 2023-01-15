094c01d44a29152db358b9656f327d9c21d005ca
Datka <datkascnu@gmail.com>
2023-01-15 15:29:15 +0600

    Initial commit

---
+++ src/test/java/com/example/giftlist/GiftlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giftlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
